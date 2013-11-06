cd5750e6ac8b9308041a50cdca7e42a020e8402b
unknown <rookfighter@develop>
2013-11-06 10:25:14 +0100

    Initial Commit.

---
+++ src/blatt5/BullyProcess.java
@@ -0,0 +1,27 @@
+package blatt5;
+
+import disy.Process;
+
+public class BullyProcess extends Process {
+
+	protected BullyProcess(int id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void process(ElectionRequest electionRequest) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void process(ElectionResponse electionResponse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void process(ElectionTrigger electionTrigger) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
